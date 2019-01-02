c642163dc5264a7b8b7e2e7985de24fff37a5519
Monica Ivn <monicaivan25@gmail.com>
2019-01-02 18:36:51 +0200

    added file saving on firebase storage

--- authentication.js
+++ authentication.js
@@ -15,7 +15,10 @@ const textPsw = document.getElementById("textPsw");
 const loginBtn = document.getElementById("login");
 const signupBtn = document.getElementById("signup");
 const logoutBtn = document.getElementById("logout");
-
+const modalContent = document.getElementsByClassName("modal-content")[0];
+const title = document.getElementById("title");
+const gallery = document.getElementById("gallery");
+const saveFireBtn = document.getElementById("save-remote");
 function exitModal(){
 	modal.style.display = 'none';
 	txtEmail.value='';
@@ -41,13 +44,14 @@ loginBtn.addEventListener('click', e => {
 	const email = txtEmail.value;
 	const pass = textPsw.value;
 	const auth = firebase.auth();
+
 	if(validateEmail(email))
 	{
 		const promise = auth.signInWithEmailAndPassword(email, pass);
 		promise.catch(e => console.log(e.message));
 		exitModal();
 		alert('Logged in');
-
+		loggedUser = getFolderNameForEmail(email);
 	}
 });
 
@@ -74,25 +78,42 @@ logoutBtn.addEventListener('click', e => {
 
 firebase.auth().onAuthStateChanged(firebaseUser => {
 	if(firebaseUser){
+		loggedUser = firebaseUser.email;
 		console.log(firebaseUser);
-		logoutBtn.style.display = 'block';
+		title.style.display = 'block';
+		gallery.style.display = 'block';
+		modalContent.style.width = '780px';
+		txtEmail.style.display = 'none';
+		textPsw.style.display = 'none';
+		logoutBtn.style.display = 'inline-block';
 		loginBtn.style.display = 'none';
 		signupBtn.style.display = 'none';
-		writeUserData("email");
-
+		saveFireBtn.style.display = 'block';
 	} else {
 		console.log('not logged in');
+		title.style.display = 'none';
+		gallery.style.display = 'none';
+		modalContent.style.width = '400px';
+		txtEmail.style.display = 'block';
+		textPsw.style.display = 'block';
 		logoutBtn.style.display = 'none';
 		loginBtn.style.display = 'block';
 		signupBtn.style.display = 'block';
+		saveFireBtn.style.display = 'none';
+
 	}
 })
 
-var database = firebase.database();
-
-function writeUserData(email) {
-	console.log(email);
-	firebase.database().ref('users/' + email).set({
-		caca: 2
-	});
+String.prototype.hashCode = function() {
+	var hash = 0;
+	if (this.length == 0) {
+		return hash;
+	}
+	for (var i = 0; i < this.length; i++) {
+		var char = this.charCodeAt(i);
+		hash = ((hash<<5)-hash)+char;
+        hash = hash & hash; // Convert to 32bit integer
+    }
+    return hash;
 }
+
--- fileExport.js
+++ fileExport.js
@@ -30,7 +30,10 @@ chart.selectAll('cirle')
 });
 
 
-// Set-up the export buttons
+
+var loggedUser;
+
+// Local save buttons
 var savePngBtn = document.getElementById("save-as-png");
 savePngBtn.addEventListener('click', function(){
 	var svgString = getSVGString(svg.node());
@@ -44,9 +47,18 @@ var saveSvgBtn = document.getElementById("save-as-svg");
 saveSvgBtn.addEventListener('click', function(){
 	var svgString = getSVGString(svg.node());
 	var blob = new Blob([svgString], {type: "text/plain;charset=utf-8"});
-    saveAs(blob, "fractal.svg");
+	saveAs(blob, "fractal.svg");
 });
 
+//remote save button
+var saveRemoteBtn = document.getElementById("save-remote");
+saveRemoteBtn.addEventListener('click', function(){
+	var svgString = getSVGString(svg.node());
+	console.log("saving remotely");
+	var blob = new Blob([svgString], {type: "text/plain;charset=utf-8"});
+	var storageRef = firebase.storage().ref(loggedUser + '/titlu2' + '.svg').put(blob);
+
+});
 
 function getSVGString( svgNode ) {
 	svgNode.setAttribute('xlink', 'http://www.w3.org/1999/xlink');
@@ -58,7 +70,6 @@ function getSVGString( svgNode ) {
 
 	return svgString;
 }
-
 
 function svgString2Image( svgString, width, height, format, callback ) {
 	var format = format ? format : 'png';
@@ -83,3 +94,30 @@ function svgString2Image( svgString, width, height, format, callback ) {
 	};
 	image.src = imgsrc;
 }
+
+firebase.auth().onAuthStateChanged(firebaseUser => {
+	if(firebaseUser){
+		loggedUser = firebaseUser.email;
+		console.log(firebaseUser);
+		title.style.display = 'block';
+		gallery.style.display = 'block';
+		modalContent.style.width = '780px';
+		txtEmail.style.display = 'none';
+		textPsw.style.display = 'none';
+		logoutBtn.style.display = 'inline-block';
+		loginBtn.style.display = 'none';
+		signupBtn.style.display = 'none';
+		saveFireBtn.style.display = 'block';
+	} else {
+		console.log('not logged in');
+		title.style.display = 'none';
+		gallery.style.display = 'none';
+		modalContent.style.width = '400px';
+		txtEmail.style.display = 'block';
+		textPsw.style.display = 'block';
+		logoutBtn.style.display = 'none';
+		loginBtn.style.display = 'block';
+		signupBtn.style.display = 'block';
+		saveFireBtn.style.display = 'none';
+	}
+})
