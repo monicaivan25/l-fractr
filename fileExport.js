632b9d7bf5c5705a4188569fe47c4b29b8bb06d8
Monica Ivn <monicaivan25@gmail.com>
2019-01-02 14:10:51 +0200

    added svg file save

--- fileExport.js
+++ fileExport.js
@@ -30,10 +30,9 @@ chart.selectAll('cirle')
 });
 
 
-// Set-up the export button
-
-var savebtn = document.getElementById("savebtn");
-savebtn.addEventListener('click', function(){
+// Set-up the export buttons
+var savePngBtn = document.getElementById("save-as-png");
+savePngBtn.addEventListener('click', function(){
 	var svgString = getSVGString(svg.node());
 	svgString2Image( svgString, 2*width, 2*height, 'png', save ); // passes Blob and filesize String to the callback
 	function save( dataBlob, filesize ){
@@ -41,13 +40,16 @@ savebtn.addEventListener('click', function(){
 	}
 });
 
+var saveSvgBtn = document.getElementById("save-as-svg");
+saveSvgBtn.addEventListener('click', function(){
+	var svgString = getSVGString(svg.node());
+	var blob = new Blob([svgString], {type: "text/plain;charset=utf-8"});
+    saveAs(blob, "fractal.svg");
+});
 
-// Below are the functions that handle actual exporting:
-// getSVGString ( svgNode ) and svgString2Image( svgString, width, height, format, callback )
+
 function getSVGString( svgNode ) {
 	svgNode.setAttribute('xlink', 'http://www.w3.org/1999/xlink');
-	var cssStyleText = getCSSStyles( svgNode );
-	appendCSS( cssStyleText, svgNode );
 
 	var serializer = new XMLSerializer();
 	var svgString = serializer.serializeToString(svgNode);
@@ -55,64 +57,6 @@ function getSVGString( svgNode ) {
 	svgString = svgString.replace(/NS\d+:href/g, 'xlink:href'); // Safari NS namespace fix
 
 	return svgString;
-
-	function getCSSStyles( parentElement ) {
-		var selectorTextArr = [];
-
-		// Add Parent element Id and Classes to the list
-		selectorTextArr.push( '#'+parentElement.id );
-		for (var c = 0; c < parentElement.classList.length; c++)
-			if ( !contains('.'+parentElement.classList[c], selectorTextArr) )
-				selectorTextArr.push( '.'+parentElement.classList[c] );
-
-		// Add Children element Ids and Classes to the list
-		var nodes = parentElement.getElementsByTagName("*");
-		for (var i = 0; i < nodes.length; i++) {
-			var id = nodes[i].id;
-			if ( !contains('#'+id, selectorTextArr) )
-				selectorTextArr.push( '#'+id );
-
-			var classes = nodes[i].classList;
-			for (var c = 0; c < classes.length; c++)
-				if ( !contains('.'+classes[c], selectorTextArr) )
-					selectorTextArr.push( '.'+classes[c] );
-			}
-
-		// Extract CSS Rules
-		var extractedCSSText = "";
-		for (var i = 0; i < document.styleSheets.length; i++) {
-			var s = document.styleSheets[i];
-
-			try {
-				if(!s.cssRules) continue;
-			} catch( e ) {
-				if(e.name !== 'SecurityError') throw e; // for Firefox
-				continue;
-			}
-
-			var cssRules = s.cssRules;
-			for (var r = 0; r < cssRules.length; r++) {
-				if ( contains( cssRules[r].selectorText, selectorTextArr ) )
-					extractedCSSText += cssRules[r].cssText;
-			}
-		}
-
-
-		return extractedCSSText;
-
-		function contains(str,arr) {
-			return arr.indexOf( str ) === -1 ? false : true;
-		}
-
-	}
-
-	function appendCSS( cssText, element ) {
-		var styleElement = document.createElement("style");
-		styleElement.setAttribute("type","text/css"); 
-		styleElement.innerHTML = cssText;
-		var refNode = element.hasChildNodes() ? element.children[0] : null;
-		element.insertBefore( styleElement, refNode );
-	}
 }
 
 
@@ -137,6 +81,5 @@ function svgString2Image( svgString, width, height, format, callback ) {
 		});
 
 	};
-
 	image.src = imgsrc;
 }
